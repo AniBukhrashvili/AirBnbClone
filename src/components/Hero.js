a6cb0e627191931d089013b9f6dc0c8f6ba70e6b
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2022-02-07 13:31:17 +0400

    init

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero() {
+    return(
+        <section>
+            <img src="../images/photo-grid.png" className="photogrid"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by
+            one-of-a-kind hosts-al without leaving home.</p>
+        </section>
+    )
+}
